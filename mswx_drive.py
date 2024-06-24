e03d742875d1fb7c519cd8cd0e53be7b2662486d
CarlosNasayo <carlosnasayov@gmail.com>
2024-06-24 11:59:04 -0500

    fix download files using service account

--- mswx_drive.py
+++ mswx_drive.py
@@ -1,50 +1,44 @@
 import os
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
 from datetime import datetime, timedelta
 
 class GoogleDriveManager:
-    def __init__(self):
-        self.drive = self.authenticate_drive()
+    def __init__(self, credentials_file):
+        self.drive = self.authenticate_drive(credentials_file)
         
-    def authenticate_drive(self):
-        gauth = GoogleAuth()
-        gauth.LocalWebserverAuth()  # Autenticar y crear credenciales
-        return GoogleDrive(gauth)
+    def authenticate_drive(self, credentials_file):
+        SCOPES = ['https://www.googleapis.com/auth/drive']
+        credentials = service_account.Credentials.from_service_account_file(
+            credentials_file, scopes=SCOPES)
+        return build('drive', 'v3', credentials=credentials)
     
     def list_folders_in_folder(self, folder_id, var_mswx):
         query = f"'{folder_id}' in parents and mimeType = 'application/vnd.google-apps.folder' and trashed=false"
-        file_list = self.drive.ListFile({
-            'q': query,
-            'supportsAllDrives': True,
-            'includeItemsFromAllDrives': True
-        }).GetList()
+        results = self.drive.files().list(q=query, supportsAllDrives=True, includeItemsFromAllDrives=True).execute()
+        file_list = results.get('files', [])
         
         folders = []
         for file in file_list:
-            title = file['title']
+            title = file['name']
             if title in var_mswx.keys():
                 folders.append({'title': title, 'id': file['id']})
         
         return folders
     
     def list_files_in_daily_folder(self, folder_id, ini_date, fin_date, var_mswx, folder_title):
-        query = f"'{folder_id}' in parents and title = 'Daily' and trashed=false"
-        daily_folder = self.drive.ListFile({
-            'q': query,
-            'supportsAllDrives': True,
-            'includeItemsFromAllDrives': True
-        }).GetList()
+        query = f"'{folder_id}' in parents and name = 'Daily' and trashed=false"
+        results = self.drive.files().list(q=query, supportsAllDrives=True, includeItemsFromAllDrives=True).execute()
+        daily_folder = results.get('files', [])
         
         if len(daily_folder) == 1:
             daily_folder_id = daily_folder[0]['id']
             print(f"Archivos y carpetas dentro de la carpeta 'Daily' (ID: {daily_folder_id}):")
             
-            daily_files = self.drive.ListFile({
-                'q': f"'{daily_folder_id}' in parents and trashed=false",
-                'supportsAllDrives': True,
-                'includeItemsFromAllDrives': True
-            }).GetList()
+            daily_files = self.drive.files().list(
+                q=f"'{daily_folder_id}' in parents and trashed=false",
+                supportsAllDrives=True, includeItemsFromAllDrives=True).execute().get('files', [])
             
             base_download_dir = os.path.join(os.getcwd(), 'MSWX')
             if not os.path.exists(base_download_dir):
@@ -53,20 +47,27 @@ class GoogleDriveManager:
             date_range = [(ini_date + timedelta(days=i)).strftime('%Y%j') for i in range((fin_date - ini_date).days + 1)]
             
             for file in daily_files:
-                file_date_str = file['title'][:7]
-                if file_date_str in date_range and file['title'].endswith('.nc'):
-                    variable_name = file['title'].split('_')[0]
+                file_date_str = file['name'][:7]
+                if file_date_str in date_range and file['name'].endswith('.nc'):
+                    variable_name = file['name'].split('_')[0]
                     variable_download_dir = os.path.join(base_download_dir, folder_title)
                     if not os.path.exists(variable_download_dir):
                         os.makedirs(variable_download_dir)
                     
-                    self.download_file(file['id'], file['title'], variable_download_dir)
+                    self.download_file(file['id'], file['name'], variable_download_dir)
         else:
             print(f"No se encontró la carpeta 'Daily' dentro de la carpeta con ID: {folder_id}")
 
     def download_file(self, file_id, file_name, download_folder_path):
-        file = self.drive.CreateFile({'id': file_id})
-        file.GetContentFile(os.path.join(download_folder_path, file_name))
+        request = self.drive.files().get_media(fileId=file_id)
+        file_path = os.path.join(download_folder_path, file_name)
+        with open(file_path, 'wb') as file:
+            downloader = MediaIoBaseDownload(file, request)
+            done = False
+            while not done:
+                status, done = downloader.next_chunk()
+                if status:
+                    print(f"Descargado {int(status.progress() * 100)}%.")
         print(f"Descargado archivo: {file_name}")
 
 class FolderManager:
@@ -84,7 +85,8 @@ class FolderManager:
             self.drive_manager.list_files_in_daily_folder(folder['id'], self.ini_date, self.fin_date, self.var_mswx, folder['title'])
 
 if __name__ == "__main__":
-    drive_manager = GoogleDriveManager()
+    credentials_file = './credentials.json'  # Reemplazar por la ruta
+    drive_manager = GoogleDriveManager(credentials_file)
     folder_id = "14no0Wkoat3guyvVnv-LccXOEoxQqDRy7"
     ini_date = datetime(2024, 4, 8).date()
     fin_date = datetime(2024, 4, 18).date()
